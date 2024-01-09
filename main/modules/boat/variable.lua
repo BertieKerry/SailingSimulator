e1cc4564a769e03efb671beda9dbbc4ef2d89bba
Bertie Kerry <113768136+BertieKerry@users.noreply.github.com>
2024-01-09 12:05:11 +0000

    AI FIXED

--- main/modules/boat/variable.lua
+++ main/modules/boat/variable.lua
@@ -16,6 +16,7 @@ M.id = nil
 
 M.velo = {}
 
+
 function M.modifysAngle(id, value)
 	M.sAngle[id] = value
 	--print(id,value)
@@ -58,6 +59,22 @@ end
 function M.modifyvelo(id, value)
 	M.velo[id] = value
 	--print(id,value)
+end
+
+M.ran20 = {2, 19, 14, 12, 17, 2, 17, 12, 6, 3, 2, 10, 20, 19, 12, 15, 17, 6, 1, 13}
+
+
+function M.popran20()
+	table.remove(M.ran20,1)
+end
+
+M.ran40 = {10, 30, 11, 20, 5, 35, 34, 16, 32, 15, 39, 38, 17, 32, 9, 24, 2, 20, 34, 12, 12, 22, 8, 3, 23, 4, 21, 35, 35, 29, 1, 37, 8, 16, 36, 34, 39, 8, 26, 32}
+
+
+
+function M.popran40()
+	table.remove(M.ran40,1)
+	
 end
 
 return M
--- main/modules/courses.lua
+++ main/modules/courses.lua
@@ -2,6 +2,6 @@ local M = {}
 
 
 --e.g. {number of middle marks,{start line}, mark1, marketc, finish 
-M.courses = {{1,vmath.vector3(0, 0, 0),vmath.vector3(500, 2000, 0),vmath.vector3(2000, 2000, 0)},{3,vmath.vector3(0, 0, 0),vmath.vector3(500, 2000, 0),vmath.vector3(2000, 2000, 0),vmath.vector3(2000, 0, 0),vmath.vector3(3000, 0, 0)}}
+M.courses = {{1,vmath.vector3(0, 0, 0),vmath.vector3(500, 2000, 0),vmath.vector3(2000, 2000, 0)},{3,vmath.vector3(0, 0, 0),vmath.vector3(500, 2000, 0),vmath.vector3(2000, 2000, 0),vmath.vector3(2000, 0, 0),vmath.vector3(3000, 0, 0)},{1,vmath.vector3(0, 0, 0),vmath.vector3(500, 2000, 0),vmath.vector3(-2000, 2000, 0)},{2,vmath.vector3(0, 0, 0),vmath.vector3(500, 2000, 0),vmath.vector3(500, -2000, 0),vmath.vector3(2000, -2000, 0)}}
 
 return M
